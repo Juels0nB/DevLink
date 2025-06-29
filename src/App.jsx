a45c98a6891450d16bbb49b9f50862dda9a019d7
Juels0nB <juelsonbalanga@gmail.com>
2025-06-29 21:13:28 +0100

    Update branding, add react-icons, and enhance UI
    Replaced favicon and app title to 'DevLink', added a new Dl.png icon, and updated ProfileCard to use a username and description instead of age/location. Integrated react-icons for social links in List component, improving visual appeal. Updated dependencies to include react-icons.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-700 flex flex-col items-center justify-center gap-8 p-4">
       <ProfileCard
-        name={"Juelson Pinto Balanga"}
-        age={19}
-        location={"Portugal"}
+        name={"@jpb.dev"}
+        description={"Front-end Developer, passionate about technology and programming."}
       />
       <List />
     </div>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,3 +1,4 @@
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 import ButtonLink from "../ButtoLink/ButtoLink";
 
 function List() {
@@ -5,21 +6,24 @@ function List() {
     <div className="flex flex-col gap- w-full max-w-md mx-auto">
       <ul className="flex flex-col gap-4">
         <ButtonLink
-          className="bg-purple-600"
+          className="bg-purple-600/80"
           href="https://www.instagram.com/jpb.dev"
         >
+          <FaInstagram className="mr-2" />
           Instagram
         </ButtonLink>
         <ButtonLink
-          className="bg-blue-500/80"
+          className="bg-blue-700/80"
           href="https://github.com/Juels0nB"
         >
+          <FaGithub className="mr-2" />
           Github
         </ButtonLink>
         <ButtonLink
           className="bg-blue-500/80"
           href="https://www.linkedin.com/in/juelson-balanga-219496349/"
         >
+          <FaLinkedin className="mr-2" />
           Linkdin
         </ButtonLink>
       </ul>
